a5100f7347cb163c7b892eb490a99b6c44a12146
JP Douma <jpdouma@gmail.com>
2022-12-28 18:33:52 +0300

    loop thru args and find index. check if index exists, otherwise remove arg with index from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+  let item;
+  for (let arg of args) {
+    let index = arr.indexOf(arg);
+    if (index === -1) continue;
+    arr.splice(index,1);
+  }
+  return arr;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
