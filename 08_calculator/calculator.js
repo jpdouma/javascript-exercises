9f9a63fc6eb9d2993738524bb90b66fc532450ff
JP Douma <jpdouma@gmail.com>
2022-12-29 12:40:11 +0300

    submit finished exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,34 @@
-const add = function() {
-	
+const add = function(...args) {
+	return args.reduce((acc, arg) => acc + arg, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+	return args.reduce((acc, arg) => acc - arg);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acc, elem) => acc + elem, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return array.reduce((acc, elem) => acc * elem, 1);
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	return base ** power; 
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = num;
+  if (num === 0 || num === 1) 
+    return 1; 
+  while (num > 1) { 
+    num--;
+    result *= num;
+  }
+  return result;
 };
+
 
 // Do not edit below this line
 module.exports = {
