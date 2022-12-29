a72f355f95cbddfa3339cbacfd81abf4380a1b86
JP Douma <jpdouma@gmail.com>
2022-12-29 10:47:57 +0300

    submit 05 thru 07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+  if (start < 0 ||typeof start !== "number" || 
+    typeof end !== "number") return "ERROR";
+  if (end < start) {[end, start] = [start, end]};
+  let sum = 0;
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let conversion = (fahrenheit -32) * 5/9;
+  return (Math.round(conversion * 10) / 10).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let conversion = celcius * 5/9 + 32;
+  return (Math.round(conversion * 10) / 10).toFixed(1);
 };
 
 // Do not edit below this line
